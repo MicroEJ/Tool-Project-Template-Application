d17757c0f5567fb4cfdd6780ddaa96801e113d74
Denis BEDIN <denis.bedin@microej.com>
2024-09-25 12:09:25 +0200

    update Gradle version and edc

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ microej {
 }
 
 dependencies {
-    implementation("ej.api:edc:1.3.5")
+    implementation("ej.api:edc:1.3.6")
 
     //Uncomment the microejVee dependency to set the VEE Port or Kernel to use
     //microejVee("com.mycompany:myvee:1.0.0")
